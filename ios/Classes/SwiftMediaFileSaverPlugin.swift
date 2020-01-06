8a036c4a0a8d97786bd9de26a233d52bf2636cf1
jdeguzman <jdeguzman@freelancer.com>
2020-01-06 18:26:52 +0800

    Add request authorization when authorization is undetermined initially.

--- ios/Classes/SwiftMediaFileSaverPlugin.swift
+++ ios/Classes/SwiftMediaFileSaverPlugin.swift
@@ -15,17 +15,15 @@ public class SwiftMediaFileSaverPlugin: NSObject, FlutterPlugin {
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
       self.result = result
       if call.method == "saveImage" {
-        self.checkPhotoLibraryAuthorization()
-
-        if(self.isAuthorized) {
+        self.checkAuthorization()
+        if (self.isAuthorized) {
           guard let imageData = (call.arguments as? FlutterStandardTypedData)?.data, let image = UIImage(data: imageData) else { return }
           // TODO: Add MethodChannel to handle callbacks and errors and return show it to users (if deemed necessary).
           UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
         }
       } else if (call.method == "saveFile") {
-        self.checkPhotoLibraryAuthorization()
-
-        if(self.isAuthorized) {
+        self.checkAuthorization()
+        if (self.isAuthorized) {
            guard let path = call.arguments as? String else { return }
             if (isImageFile(filename: path)) {
                 if let image = UIImage(contentsOfFile: path) {
@@ -56,18 +54,21 @@ public class SwiftMediaFileSaverPlugin: NSObject, FlutterPlugin {
             || filename.hasSuffix(".GIF")
     }
 
-    func checkPhotoLibraryAuthorization() {
+    func checkAuthorization() {
       let status = PHPhotoLibrary.authorizationStatus()
-      switch status {
-        case .authorized:
-          print("authorized")
-          self.isAuthorized = true
-        case .denied, .restricted:
-          print("authorization denied")
-          // TODO: Add a channel to handle the denied status of the authorization
-        case .notDetermined:
-          print("authorization not determined")
-          // TODO: Add a channel to handle the undetermined status of the authorization
+      if status == .notDetermined {
+        PHPhotoLibrary.requestAuthorization({ status in
+          if status == .authorized {
+            self.isAuthorized = true
+          } else {
+            self.isAuthorized = false
+            // TODO: Add a channel to handle the undetermined status of the authorization
+          }
+        })
+      } else if status == .denied || status == .restricted {
+        self.isAuthorized = false
+      } else {
+        self.isAuthorized = true
       }
     }
 }
